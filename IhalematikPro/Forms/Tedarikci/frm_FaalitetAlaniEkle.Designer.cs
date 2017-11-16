53d09b450fe60a2089b6f4febc0726e685868892
Feyzullah Gezici <Feyzullahgezici@gmail.com>
2017-11-16 10:32:06 +0300

    Faaliyet Alani Altyapisi olusturuldu

--- IhalematikPro/Forms/Tedarikci/frm_FaalitetAlaniEkle.Designer.cs
+++ IhalematikPro/Forms/Tedarikci/frm_FaalitetAlaniEkle.Designer.cs
@@ -54,8 +54,7 @@
             // txtUnvan
             // 
             this.txtUnvan.EnterMoveNextControl = true;
-            this.txtUnvan.Location = new System.Drawing.Point(123, 50);
-            this.txtUnvan.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUnvan.Location = new System.Drawing.Point(82, 34);
             this.txtUnvan.Name = "txtUnvan";
             this.txtUnvan.Properties.Appearance.BackColor = System.Drawing.Color.White;
             this.txtUnvan.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
@@ -63,17 +62,16 @@
             this.txtUnvan.Properties.Appearance.Options.UseBackColor = true;
             this.txtUnvan.Properties.Appearance.Options.UseFont = true;
             this.txtUnvan.Properties.Appearance.Options.UseForeColor = true;
-            this.txtUnvan.Size = new System.Drawing.Size(546, 26);
+            this.txtUnvan.Size = new System.Drawing.Size(364, 20);
             this.txtUnvan.TabIndex = 0;
             // 
             // labelControl2
             // 
             this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.labelControl2.Appearance.ForeColor = System.Drawing.Color.Black;
-            this.labelControl2.Location = new System.Drawing.Point(15, 54);
-            this.labelControl2.Margin = new System.Windows.Forms.Padding(4);
+            this.labelControl2.Location = new System.Drawing.Point(10, 37);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(100, 19);
+            this.labelControl2.Size = new System.Drawing.Size(68, 13);
             this.labelControl2.TabIndex = 97;
             this.labelControl2.Text = "Faaliyet Alanı:";
             // 
@@ -89,10 +87,9 @@
             this.groupControl2.Controls.Add(this.txtUnvan);
             this.groupControl2.Controls.Add(this.labelControl2);
             this.groupControl2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupControl2.Location = new System.Drawing.Point(0, 54);
-            this.groupControl2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupControl2.Location = new System.Drawing.Point(0, 37);
             this.groupControl2.Name = "groupControl2";
-            this.groupControl2.Size = new System.Drawing.Size(674, 178);
+            this.groupControl2.Size = new System.Drawing.Size(449, 122);
             this.groupControl2.TabIndex = 36;
             this.groupControl2.Text = "Faaliyet Bilgileri";
             // 
@@ -104,12 +101,12 @@
             this.btnSave.Appearance.Options.UseForeColor = true;
             this.btnSave.Image = global::IhalematikProUI.Properties.Resources.Save_as_icon__1_;
             this.btnSave.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
-            this.btnSave.Location = new System.Drawing.Point(549, 102);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Location = new System.Drawing.Point(360, 70);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(120, 50);
+            this.btnSave.Size = new System.Drawing.Size(85, 34);
             this.btnSave.TabIndex = 93;
             this.btnSave.Text = "Kaydet";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnKapat
             // 
@@ -117,10 +114,9 @@
             this.btnKapat.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnKapat.Image = global::IhalematikProUI.Properties.Resources.Actions_dialog_close_icon;
             this.btnKapat.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
-            this.btnKapat.Location = new System.Drawing.Point(610, 0);
-            this.btnKapat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKapat.Location = new System.Drawing.Point(406, 0);
             this.btnKapat.Name = "btnKapat";
-            this.btnKapat.Size = new System.Drawing.Size(64, 54);
+            this.btnKapat.Size = new System.Drawing.Size(43, 37);
             this.btnKapat.TabIndex = 1;
             // 
             // panelControl1
@@ -132,9 +128,8 @@
             this.panelControl1.Controls.Add(this.panelControl2);
             this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelControl1.Location = new System.Drawing.Point(0, 0);
-            this.panelControl1.Margin = new System.Windows.Forms.Padding(4);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(674, 54);
+            this.panelControl1.Size = new System.Drawing.Size(449, 37);
             this.panelControl1.TabIndex = 35;
             // 
             // panelControl2
@@ -146,9 +141,8 @@
             this.panelControl2.Controls.Add(this.btnKapat);
             this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelControl2.Location = new System.Drawing.Point(0, 0);
-            this.panelControl2.Margin = new System.Windows.Forms.Padding(4);
             this.panelControl2.Name = "panelControl2";
-            this.panelControl2.Size = new System.Drawing.Size(674, 54);
+            this.panelControl2.Size = new System.Drawing.Size(449, 37);
             this.panelControl2.TabIndex = 2;
             // 
             // labelControl1
@@ -159,11 +153,10 @@
             this.labelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
             this.labelControl1.LineColor = System.Drawing.Color.Red;
             this.labelControl1.LineStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
-            this.labelControl1.Location = new System.Drawing.Point(4, 9);
-            this.labelControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelControl1.Location = new System.Drawing.Point(3, 6);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.ShowLineShadow = false;
-            this.labelControl1.Size = new System.Drawing.Size(354, 37);
+            this.labelControl1.Size = new System.Drawing.Size(231, 24);
             this.labelControl1.TabIndex = 0;
             this.labelControl1.Text = "FAALİYET ALANI EKLE";
             this.labelControl1.UseMnemonic = false;
@@ -171,15 +164,14 @@
             // frm_FaalitetAlaniEkle
             // 
             this.AcceptButton = this.btnSave;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnKapat;
-            this.ClientSize = new System.Drawing.Size(674, 232);
+            this.ClientSize = new System.Drawing.Size(449, 159);
             this.ControlBox = false;
             this.Controls.Add(this.groupControl2);
             this.Controls.Add(this.panelControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frm_FaalitetAlaniEkle";
--- IhalematikPro/Forms/Tedarikci/frm_FaalitetAlaniEkle.cs
+++ IhalematikPro/Forms/Tedarikci/frm_FaalitetAlaniEkle.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using IhalematikProBL.Entity;
+using IhalematikProBL.Provider;
 
 namespace IhalematikProUI.Forms
 {
@@ -16,6 +18,26 @@ namespace IhalematikProUI.Forms
         public frm_FaalitetAlaniEkle()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUnvan.Text.Trim()))
+            {
+                SupplierSegment supplierSegment = new SupplierSegment();
+                supplierSegment.Name = txtUnvan.Text.Trim();
+                List<SupplierSegment> existingItem = SupplierSegmentProvider.Instance.GetItems("Name", supplierSegment.Name);
+
+                if (existingItem.Count == 0)
+                {
+                    SupplierSegmentProvider.Instance.Save(supplierSegment);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Ayni kayit vardir");
+                }
+            }
         }
     }
 }
