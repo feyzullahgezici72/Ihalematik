c0ef74c9cbceef35066b7357c995e795587f791f
feyzullah gezici <feyzullhgezici@gmail.com>
2017-12-16 16:58:33 +0300

    Tüm controllere focus uygulandı

--- IhalematikPro/Forms/Calisan/frm_Calisan_Guncelleme.Designer.cs
+++ IhalematikPro/Forms/Calisan/frm_Calisan_Guncelleme.Designer.cs
@@ -185,7 +185,7 @@
             this.panel1.Location = new System.Drawing.Point(22, 110);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(406, 478);
+            this.panel1.Size = new System.Drawing.Size(418, 478);
             this.panel1.TabIndex = 34;
             // 
             // txtWorklesFonFare
