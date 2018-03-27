0b1885207e7217ae3fdb29f7e43c9d4bdc4da6f9
-adm-sbegde <-adm-sbegde@L0720014>
2018-03-27 09:05:28 +0300

    sdd

---
+++ IhalematikPro/Forms/Tedarikci/frm_TeklifAktarActionForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace IhalematikProUI.Forms.Tedarikci
+{
+    public partial class frm_TeklifAktarActionForm : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_TeklifAktarActionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
